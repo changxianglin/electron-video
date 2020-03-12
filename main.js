fbf04c1a4e38c155fc673dc1173237908265e062
changxianglin <281532612@qq.com>
2020-03-13 03:01:47 +0800

    can run demo and need to use cnpm

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
-function createWindow () {   
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // 并且为你的应用加载index.html
-  win.loadFile('index.html')
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
 
-  // 打开开发者工具
-  win.webContents.openDevTools()
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// 部分 API 在 ready 事件触发后才能使用。
-app.whenReady().then(createWindow)
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
-// code. 也可以拆分成几个文件，然后用 require 导入。
+// code. You can also put them in separate files and require them here.
