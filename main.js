e435b21f35a003ee019c92ddc10fc91648b77af5
changxianglin <281532612@qq.com>
2020-03-15 03:12:58 +0800

    selcted file

--- main.js
+++ main.js
@@ -1,49 +1,3 @@
-// // Modules to control application life and create native browser window
-// const {app, BrowserWindow} = require('electron')
-// const path = require('path')
-
-// function createWindow () {
-//   // Create the browser window.
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     backgroundColor: '#666',
-//     allowRendererProcessReuse: true,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       preload: path.join(__dirname, 'preload.js'),
-//     }
-//   })
-
-//   // and load the index.html of the app.
-//   mainWindow.loadFile('index.html')
-
-//   // Open the DevTools.
-//   // mainWindow.webContents.openDevTools()
-// }
-
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
-
-// // Quit when all windows are closed.
-// app.on('window-all-closed', function () {
-//   // On macOS it is common for applications and their menu bar
-//   // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') app.quit()
-// })
-
-// app.on('activate', function () {
-//   // On macOS it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) createWindow()
-// })
-
-// // In this file you can include the rest of your app's specific main process
-// // code. You can also put them in separate files and require them here.
-
-
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 
 class AppWindow extends BrowserWindow {
@@ -67,22 +21,27 @@ class AppWindow extends BrowserWindow {
 app.on('ready', () => {
   const mainWindow = new AppWindow({}, './renderer/index.html')
 
-  ipcMain.on('add-music-window', () => {
+  ipcMain.on('add-music-window', (event, arg) => {
     const addWindow = new AppWindow({
       width: 500,
       height: 300,
       parent: mainWindow,
     }, './renderer/add.html')
+    // addWindow.webContents.openDevTools()
   })
+  
 
-  ipcMain.on('open-music-file', () => {
+  ipcMain.on('open-music-file', (event) => {
     dialog.showOpenDialog({
       properties: ['openFile', 'multiSelections'],
       filters: [{name: 'Music', extensions: ['mp3', 'mp4']}]
     }).then(files => {
-      console.log(files)
+      if(files) {
+        event.sender.send('selected-file', files.filePaths)
+      }
     }).catch(err => {
       console.log(err)
     })
   })
+  
 })
--- renderer/index.js
+++ renderer/index.js
@@ -2,5 +2,13 @@ const { ipcRenderer } = require('electron')
 const { $ } = require('./helper')
 
 $('add-music-button').addEventListener('click', () => {
-  ipcRenderer.send('add-music-window')
+  ipcRenderer.send('add-music-window', 'add music')
 })
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   ipcRenderer.send('message', 'start event')
+//   ipcRenderer.on('reply', (event, arg) => {
+//     console.log('repose 收到', arg)
+//     document.getElementById('message').innerText = arg
+//   })
+// })
