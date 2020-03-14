682e480e2511a9702e600f95b4c1fdd7fb77060a
changxianglin <281532612@qq.com>
2020-03-15 00:28:55 +0800

    use class

--- main.js
+++ main.js
@@ -46,25 +46,32 @@
 
 const { app, BrowserWindow, ipcMain } = require('electron')
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+class AppWindow extends BrowserWindow {
+  constructor(config, fileLocation) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
     }
-  })
-  mainWindow.loadFile('./renderer/index.html')
+    const finalConfig = {...basicConfig, ...config}
+    super(finalConfig)
+    this.loadFile(fileLocation)
+    this.once('ready-to-show', () => {
+      this.show()
+    })
+  }
+}
+
+app.on('ready', () => {
+  const mainWindow = new AppWindow({}, './renderer/index.html')
 
   ipcMain.on('add-music-window', () => {
-    const addWindow = new BrowserWindow({
+    const addWindow = new AppWindow({
       width: 500,
       height: 300,
-      webPreferences: {
-        nodeIntegration: true,
-      },
       parent: mainWindow,
-    })
-    addWindow.loadFile('./renderer/add.html')
+    }, './renderer/add.html')
   })
 })
